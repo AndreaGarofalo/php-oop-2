84af4ea76165223f0cbd5693993d8afc8620ad31
AndreaGarofalo <andreasjobs94@outlook.it>
2023-02-27 17:23:33 +0100

    fixed food icon problem

--- models/FoodProduct.php
+++ models/FoodProduct.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/Product.php';
 class FoodProduct extends Product {
     private $ingredients;
     private $expiration;
-    public static $icon = 'fa-solid fa-bowl food';
+    public static $icon = 'fa-solid fa-bowl-food';
 
     public function __construct($name, $description, $price, Category $category, array $ingredients, $expiration) {
         parent::__construct($name, $description, $price, $category);
